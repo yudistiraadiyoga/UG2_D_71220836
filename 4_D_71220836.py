e2acf63202b5eeeb50152740782e6abe99b5de5f
yudistiraadiyoga <117887589+yudistiraadiyoga@users.noreply.github.com>
2023-02-15 19:09:14 +0700

    Add files via upload

--- 4_D_71220836.py
+++ 4_D_71220836.py
@@ -4,17 +4,18 @@ print('1. Menambah data')
 print('2. Menampilkan data')
 print('3. Keluar')
 pil = int(input('Silahkan masukkan pilihan yang Anda inginkan:'))
-if pil==1:
-    nama = input('Masukkan nama pelanggan :')
-    jenis = input('Masukkan jenis member:')
-    data = (nama, jenis)
-    print('Data sudah berhasil ditambahkan')
-elif pil==2:
-    print('-'*20)
-    print('Pelanggan        Member:')
-    for i in data:
-        print(i)
-elif pil==3:
-    print('Sistem Berhenti...')
-else:
-    print('error...')
+while pil<=3:
+    if pil==1:
+        nama = input('Masukkan nama pelanggan :')
+        jenis = input('Masukkan jenis member:')
+        data = (nama, jenis)
+        print('Data sudah berhasil ditambahkan')
+    elif pil==2:
+        print('-'*20)
+        print('Pelanggan        Member:')
+        for i in data:
+            print(i)
+    elif pil==3:
+        print('Sistem Berhenti...')
+    else:
+        print('error...')
